7d69389e1973a4f33c99c3acc5abe1e3590567aa
michaellidonni <526mlidonni@frhsd.com>
2024-12-01 02:49:29 +0000

    Final paragraph

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let item = prompt("Name a item!");
 let adjective2 = prompt("Name a adjective!");
 let pluralNoun = prompt("Name a plural noun!");
 let adjective3 = prompt("Name a adjective!");
-let verb = prompt("Name a verb!");
+let verb = prompt("Name a verb ending in -ing!");
 let place2 = prompt("Name a place!");
 let animal2 = prompt("Name a animal!");
-let verb2 = prompt("Name a verb!");
+let verb2 = prompt("Name a verb ending in -ing!");
 let adjective4 = prompt("Name a adjective!");
 
-let myStory = `<h5>One day, a ${adjective1} adventurer named ${name} decided to explore the mysterious ${place}. Armed with a trusty ${weapon} and a map, they set off on their journey. As they ventured deeper into the unknown, they suddenly encountered a ${animal} who offered them a ${item} in exchange for help. Together, they traveled through ${adjective2} forests, across vast fields of ${pluralNoun}, and over towering ${adjective3} mountains. After days of ${verb}, they finally reached a place called ${place2}, hidden in a ${adjective3} cave. But just as they were about to claim their prize, a fierce ${animal2} appeared and challenged them to a fight! With clever ${verb2}, the adventurer won the challenge and claimed the ${item}. Victorious, they returned home, ready for their next ${adjective4} adventure.</h5>`;
+let myStory = `<h5>One day, a ${adjective1} adventurer named ${name} decided to explore the mysterious ${place}. Armed with a trusty ${weapon} and a map, they set off on their journey. As they ventured deeper into the unknown, they suddenly encountered a ${animal} who offered them a ${item} in exchange for help. Together, they traveled through ${adjective2} forests, across vast fields of ${pluralNoun}, and over towering mountains. After days of ${verb}, they finally reached a place called ${place2}, hidden in a ${adjective3} cave. But just as they were about to claim their prize, a fierce ${animal2} appeared and challenged them to a fight! With clever ${verb2}, the adventurer won the challenge and claimed the ${item}. Victorious, they returned home, ready for their next ${adjective4} adventure.</h5>`;
 
 let title = `The Great Adventure of ${name}`;
 
